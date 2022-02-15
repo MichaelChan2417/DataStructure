e106d1ee11e9daa68508e9750d7a17f173b50d0f
MichaelChan2417 <45192104+MichaelChan2417@users.noreply.github.com>
2022-02-15 13:42:30 +0800

    Update Stack.cpp

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -7,47 +7,41 @@ using namespace std;
 
 // Constructor Function
 Stack::Stack(string x) {
-	stack_size = 1;
-	space.push_back(x);
-}
-
-Stack::Stack(vector<string> s) {
-	stack_size = s.size();
-	for (int i = 0; i < s.size(); i++) {
-		space.push_back(s[i]);
-	}
+	stack_size = x.size();
+    space = x;
 }
 
 
 // Push element into the stack
-void Stack::st_push(string x) {
+void Stack::st_push(char x) {
 	stack_size++;
-	space.push_back(x);
+	space = space+x;
 }
 
 
 // Pop the top element out
-string Stack::st_pop() {
+char Stack::st_pop() {
 	if (stack_size == 0) {
 		cout << "Empty Stack, No pop Value" << endl;
-		return "-1";
+		return '\0';
 	}
 
 	stack_size--;
-	string ans = space.back();
-	space.pop_back();
-	return ans;
+    char ans = space[stack_size];
+    space = space.substr(0,stack_size);
+
+    return ans;
 }
 
 
 // Return the top value in the stack
-string Stack::st_top() {
+char Stack::st_top() {
 	if (stack_size == 0) {
 		cout << "Empty Stack, No top Value" << endl;
-		return "-1";
+		return '\0';
 	}
 
-	string ans = space.back();
+	char ans = space[stack_size-1];
 	return ans;
 }
 
@@ -55,4 +49,33 @@ string Stack::st_top() {
 // the size of the stack
 int Stack::len() {
 	return stack_size;
+}
+
+
+// Print the Stack
+void Stack::Print_Stack(){
+    for(int i=0;i<space.size();i++){
+        cout<<space[i]<<"-";
+    }
+    cout<<endl;
+}
+
+int main(){
+    string temp = "12345";
+    Stack st(temp);
+
+    st.Print_Stack();
+    char ad = st.st_pop();
+    cout<<ad<<endl;
+    st.Print_Stack();
+
+    char ap = st.st_top();
+    cout<<ap<<endl;
+    st.Print_Stack();
+
+    st.st_push('i');
+    st.Print_Stack();
+    
+    system("pause");
+    return 0;
 }
